af97ec680157245fb3782006ade0268903fa372c
Savi mhatre <savitri.mhatre@pridevel.com>
2016-01-21 08:15:07 +0000

    added profile update action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -22,6 +22,18 @@ class ProfilesController < ApplicationController
     @profile = @user.profile
   end
   
+  # whenever user makes chnages to the profile it come to "Update" action created below #
+def update
+  @user = User.find( params[:user_id] )           #establish which user profile we are editing 
+  @profile = @user.profile                        #store user profile data in @profile variable 
+  if @profile.update_attributes(profile_params)   # checking in db if data is updated
+    flash[:success] = "Profile Updated!"
+    redirect_to user_path( params[:user_id] )     # display the edited profile
+  else
+    render action: :edit                         #redirecting user to edit page
+  end
+end
+  
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
