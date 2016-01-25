d86e987358550cdf05eb0014c93e7573dcea995e
Savi mhatre <savitri.mhatre@pridevel.com>
2016-01-25 08:12:56 +0000

    modified create contacts file

--- db/migrate/20160106162351_create_contacts.rb
+++ db/migrate/20160106162351_create_contacts.rb
@@ -1,6 +1,7 @@
 class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
+      #adding columns to the model/database
       t.string :name         # store name
       t.string :email        #store email
       t.text :comments       #store large text...
