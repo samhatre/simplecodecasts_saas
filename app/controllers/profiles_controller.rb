d931d57fb757a4391879fb7c1a13d5c854b36dd5
Savi mhatre <savitri.mhatre@pridevel.com>
2016-01-25 11:26:23 +0000

    added filefields for image upload on profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,13 +1,13 @@
 # controllers files are always named in plural form
 class ProfilesController < ApplicationController
- before_action :authenticate_user!     #this allows user to access his own profile if he is logged in
+before_action :authenticate_user!     #this allows user to access his own profile if he is logged in
   before_action :only_current_user     #this prohibits user from making chnages in other users profile.
   def new
     # form where a user can fill out their own profile.
     @user = User.find( params[:user_id] )
     @profile = Profile.new
   end
-  
+
   def create 
     @user = User.find( params[:user_id] )
     @profile = @user.build_profile(profile_params)
@@ -18,12 +18,12 @@ class ProfilesController < ApplicationController
       render action: :new
     end
   end
-  
-  def edit 
-    @user = User.find( params[:user_id] )
-    @profile = @user.profile
-  end
-  
+
+def edit
+  @user = User.find( params[:user_id] )
+  @profile = @user.profile
+end
+
   # whenever user makes chnages to the profile it come to "Update" action created below #
 def update
   @user = User.find( params[:user_id] )           #establish which user profile we are editing 
@@ -36,13 +36,17 @@ def update
   end
 end
   
+
+  
   private
     def profile_params
-      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+      params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
     end
     
     def only_current_user
-        @user = User.find(params(:user_id))
+       @user = User.find( params[:user_id] )  
         redirect_to(root_url) unless @user == current_user  #redirecting the person to home page
     end
 end
+
+  
