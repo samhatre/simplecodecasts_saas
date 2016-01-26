0d01e282d8dce371bf4dd8e8bf09039d0c6c559d
Savi mhatre <savitri.mhatre@pridevel.com>
2016-01-26 08:02:11 +0000

    added index page and upated link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!  
   
+  def index
+    
+  end
+  
   def show
     @user = User.find( params[:id] )
     #variable = db.find( parameter with id) ... explanation for line 3
